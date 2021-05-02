ac0445a8dd16ad0652e9f32d3d28473df031e966
anzhella-pankratova <anzhella.pankratova@intel.com>
2021-05-02 14:22:06 +0300

    Update FPTAS

--- Lab2/algorithms/fptas.py
+++ Lab2/algorithms/fptas.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .costs_dp import CostsDP
+from .two_approx import TwoApprox
 
 
 class FPTAS:
@@ -7,7 +8,8 @@ class FPTAS:
     def __init__(self, capacity, item_weights, item_costs, eps=0.1):
         self.item_costs = np.array(item_costs)
         self.N = len(item_weights)
-        self.alpha = (self.item_costs.max() * eps) / self.N  # max_value should be replaced by C_2A
+        c_2a = TwoApprox(capacity, item_weights, item_costs).solve()[0].knapsack_cost
+        self.alpha = (c_2a * eps) / self.N
         self.new_costs = np.floor(self.item_costs / self.alpha).astype(np.uint32)
         self.dp = CostsDP(capacity, np.array(item_weights), self.new_costs)
         self.operations = 0
