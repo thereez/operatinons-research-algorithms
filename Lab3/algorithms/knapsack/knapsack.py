ed94d4a04c336717dde1616b629b33d05dedc857
thereez <aapyanzin@edu.hse.ru>
2021-05-18 23:42:57 +0300

    added genetic algorithm for knapsack

---
+++ Lab3/algorithms/knapsack/knapsack.py
@@ -0,0 +1,80 @@
+import numpy as np
+import copy
+import random
+from random import randint
+
+
+def get_initial_population(cromosome_len, population_size):
+    population_shape = (population_size, cromosome_len)
+    population = np.random.randint(2, size=population_shape).astype(int)
+    return population
+
+
+def fitness(population, weights, costs, capacity):
+    new_population = copy.deepcopy(population)
+    fitness = np.empty(new_population.shape[0])
+    for i in range(new_population.shape[0]):
+        current_costs = np.sum(new_population[i] * costs)
+        current_weights = np.sum(new_population[i] * weights)
+        while current_weights > capacity:
+            indexes = np.where(new_population[i] == 1)[0]
+            index = np.random.choice(indexes)
+            new_population[i][index] = 0
+            current_costs = np.sum(new_population[i] * costs)
+            current_weights = np.sum(new_population[i] * weights)
+        fitness[i] = current_costs
+    return fitness.astype(int), new_population
+
+
+def best_solution(population, weights, costs, capacity):
+    costs = np.dot(population, costs)
+    weights = np.dot(population, weights)
+    indexes = np.where(weights <= capacity)[0]
+    best_costs = -1
+    result = None
+    for index in indexes:
+        if costs[index] > best_costs:
+            result = (population[index], weights[index], costs[index])
+            best_costs = costs[index]
+    return result
+
+
+def rws_selection(population, fitness, num_parents):
+    fitness = list(fitness)
+    survive_chances = [fit_value / sum(fitness) for fit_value in fitness]
+    new_generation = random.choices(population, weights=survive_chances, k=num_parents)
+    new_generation = np.asarray(new_generation)
+    return new_generation
+
+
+def crossover(parents, num_offsprings, crossover_rate):
+    offsprings = np.empty((num_offsprings, parents.shape[1]))
+    i = 0
+    while i < num_offsprings:
+        crossover_point = int(np.random.uniform(1, parents.shape[1]))
+        is_crossover = np.random.binomial(1, crossover_rate)
+        if not is_crossover:
+            continue
+        p1_index = i % parents.shape[0]
+        p2_index = (i + 1) % parents.shape[0]
+        offsprings[i, 0:crossover_point] = parents[p1_index, 0:crossover_point]
+        offsprings[i, crossover_point:] = parents[p2_index, crossover_point:]
+        i += 1
+    return offsprings
+
+
+def mutation(offsprings, mutation_rate):
+    mutants = np.empty((offsprings.shape))
+    for i in range(mutants.shape[0]):
+        mutants[i, :] = offsprings[i, :]
+        is_mutation = np.random.binomial(1, mutation_rate)
+        if not is_mutation:
+            continue
+        int_random_value = randint(0, offsprings.shape[1] - 1)
+        if mutants[i, int_random_value] == 0:
+            mutants[i, int_random_value] = 1
+        else:
+            mutants[i, int_random_value] = 0
+    return mutants
+
+
