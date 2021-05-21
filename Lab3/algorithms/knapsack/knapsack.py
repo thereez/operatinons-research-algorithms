2c7dce1062f9089da1ab2054256ccf4efea80978
thereez <aapyanzin@edu.hse.ru>
2021-05-21 19:33:07 +0300

    fixed results for knapsack

--- Lab3/algorithms/knapsack/knapsack.py
+++ Lab3/algorithms/knapsack/knapsack.py
@@ -1,7 +1,9 @@
 import numpy as np
 import copy
 import random
+import codecs
 from random import randint
+from time import perf_counter
 
 
 def get_initial_population(cromosome_len, population_size):
@@ -76,5 +78,51 @@ def mutation(offsprings, mutation_rate):
         else:
             mutants[i, int_random_value] = 0
     return mutants
+
+
+def parse_data(file_num):
+    capacity = codecs.open(f"data_knapsack/p0{file_num}_c.txt", "r", "utf_8_sig").read()
+    weights = codecs.open(f"data_knapsack/p0{file_num}_w.txt", "r", "utf_8_sig").read()
+    costs = codecs.open(f"data_knapsack/p0{file_num}_p.txt", "r", "utf_8_sig").read()
+    optimal = codecs.open(f"data_knapsack/p0{file_num}_s.txt", "r", "utf_8_sig").read()
+
+    data = {
+        'capacity': int(capacity.strip()),
+        'weights': [int(w.strip()) for w in weights.splitlines()],
+        'costs': [int(c.strip()) for c in costs.splitlines()],
+        'optimal': [int(i.strip()) for i in optimal.splitlines()],
+    }
+    return data
+
+
+def solve(data, crossover_rate, mutation_rate, epochs, population_size):
+    costs = data['costs']
+    weights = data['weights']
+    capacity = data['capacity']
+    best_costs = -1
+    best_result = None
+    start_time = perf_counter()
+    population = get_initial_population(len(weights), population_size)
+    num_parents = int(len(population) / 2)
+    num_offsprings = len(population) - num_parents
+    for i in range(epochs):
+        f, p = fitness(population, weights, costs, capacity)
+        new_gen = rws_selection(p, f, num_parents)
+        offsprings = crossover(new_gen, num_offsprings, crossover_rate)
+        mutants = mutation(offsprings, mutation_rate)
+        population[0:new_gen.shape[0], :] = new_gen
+        population[new_gen.shape[0]:, :] = mutants
+        result = best_solution(population, weights, costs, capacity)
+        if result[2] > best_costs:
+            best_result = result
+    f, p = fitness(population, weights, costs, capacity)
+    result = best_solution(p, weights, costs, capacity)
+    if result[2] > best_costs:
+        best_result = result
+    end_time = perf_counter()
+    print('Best answer:', best_result[0])
+    print('Best capacity:', best_result[1])
+    print('Best costs:', best_result[2])
+    print('Execution time: %.4f' % (end_time - start_time))
 
 
